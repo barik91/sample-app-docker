d2446bb7f6e65cd74195053a8e26297ce030b66c
Soumya Ranjan <124597907+barik91@users.noreply.github.com>
2023-08-21 22:04:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => res.send(" <b> welcome to aws cloud and Hello World!</b>"));
+app.get("/", (req, res) => res.send("<!doctype html>
+<html>
+  <head>
+    <title>This is the title of the webpage!</title>
+  </head>
+  <body>
+    <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
+  </body>
+</html>"));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
