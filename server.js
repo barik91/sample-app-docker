dc82f494fa89bc9e6bcb43022880df0974a30644
Soumya Ranjan <124597907+barik91@users.noreply.github.com>
2023-08-21 22:24:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,7 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => res.send("<!doctype html>
-<html>
-  <head>
-    <title>This is the title of the webpage!</title>
-  </head>
-  <body>
-    <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
-  </body>
-</html>"));
+app.get("/", (req, res) => res.send("welcome to India"));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
